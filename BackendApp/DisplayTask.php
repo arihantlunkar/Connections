63d3b4c2bc850a312d24695ea77bf556e158f50c
Arihant Lunkar <arihant-cse15@snu.edu.in>
2022-01-04 18:51:54 +0530

    Better select query

--- BackendApp/DisplayTask.php
+++ BackendApp/DisplayTask.php
@@ -5,7 +5,7 @@ class DisplayTask extends Task
 {
 	public function trigger() : void
 	{
-		$result = $this->mConnection->query('SELECT * FROM `biodata`, (SELECT `upload`.`fromID`, `upload`.`biodataID`, `upload`.`uploadTime` FROM `upload`, `verification` WHERE `verification`.`isVerified` = 1 AND `verification`.`isValid` = 1 AND `upload`.`biodataID` = `verification`.`biodataID`) AS VERIFICATION_DATA, (SELECT `education`.`biodataID`, GROUP_CONCAT(DISTINCT `education`.`education`) AS `educations` FROM `biodata`, `education` WHERE `biodata`.`id` = `education`.`biodataID` GROUP BY `education`.`biodataID`) AS EDUCATION_DATA WHERE `biodata`.`id` = VERIFICATION_DATA.`biodataID` AND EDUCATION_DATA.`biodataID` = `biodata`.`id` ORDER BY VERIFICATION_DATA.`uploadTime` DESC LIMIT '.$this->mJSONDecodedPOSTData->currentPosition.', 5;');
+		$result = $this->mConnection->query('SELECT * FROM `biodata`, `verification`, (SELECT `biodata`.`id`, GROUP_CONCAT(DISTINCT `education`.`education`) AS `educations` FROM `biodata`, `education` WHERE `biodata`.`id` = `education`.`biodataID` GROUP BY `biodata`.`id`) AS EDUCATION_DATA WHERE `biodata`.`id` = `verification`.`biodataID` AND `biodata`.`id` = EDUCATION_DATA.`id` AND `verification`.`isValid` = 1 ORDER BY `verification`.`verifiedTime` DESC LIMIT '.$this->mJSONDecodedPOSTData->currentPosition.', 5;');
 		$rows = array();
 		if ($result && $result->num_rows > 0) 
 		{
